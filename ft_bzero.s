8b9ce7a89f9ebb2fabab55f69e486eed4f23bd38
Emil MAMMADZADA <emammadz@e3r10p3.42.fr>
2015-10-21 17:04:09 +0200

    added ft_bzero asm function without useless register copy

--- ft_bzero.s
+++ ft_bzero.s
@@ -3,16 +3,12 @@ global ft_bzero
 section .text
 
 ft_bzero:
-	mov r8, rdi
-	mov r9, rsi
 	mov r10, 0
 
 algo:
-	mov byte [r8], 0
-	add r8, 1
+	mov byte [rdi], 0
+	add rdi, 1
 	add r10, 1
-	cmp r9, r10
+	cmp rsi, r10
 	jg algo
-
-fin:
 	ret
