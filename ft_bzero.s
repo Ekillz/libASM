d1bc196d89097c714a26dbec6046812c8bcbc587
Emil MAMMADZADA <emammadz@e3r10p3.42.fr>
2015-10-21 17:00:48 +0200

    added ft_bzero asm function

--- ft_bzero.s
+++ ft_bzero.s
@@ -3,6 +3,16 @@ global ft_bzero
 section .text
 
 ft_bzero:
-	mov rax, [esp + 4]
-	mov rdi, [esp + 8]
+	mov r8, rdi
+	mov r9, rsi
+	mov r10, 0
 
+algo:
+	mov byte [r8], 0
+	add r8, 1
+	add r10, 1
+	cmp r9, r10
+	jg algo
+
+fin:
+	ret
--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+
 void ft_bzero(void *s, size_t n);
 int main(void)
 {
+
 	char *s;
 
-	s = malloc(sizeof(char) * 7);
-	ft_bzero((void *)s, 7);
+	s = malloc(sizeof(char) * 5);
+	s[0] = 'd';
+	s[1] = 'u';
+	s[2] = 'd';
+	s[3] = 'e';
+	s[4] = '\0';
+	ft_bzero((void *)s, 4);
+	printf("%s", s);
 }
