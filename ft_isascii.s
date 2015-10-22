ef19476e1b10419b16691bd97b1494ba1d19ad56
Emil MAMMADZADA <emammadz@e3r10p3.42.fr>
2015-10-22 16:56:20 +0200

    adeed isascii

---
+++ ft_isascii.s
@@ -0,0 +1,20 @@
+gloabl ft_isascii
+
+section .text
+
+ft_isascii:
+	cmp rdi, 0
+	jae second_check
+	jmp ret0
+
+second_check:
+	cmp rdi, 126
+	jbe ret1
+
+ret0:
+	mov rax, 0
+	ret
+
+ret1:
+	mov rax, 1
+	ret
