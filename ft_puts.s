059b791d8347a508d678285cf2366c3ee31cae28
Emil MAMMADZADA <emammadz@e3r10p3.42.fr>
2015-10-23 17:21:04 +0200

    added finished asm puts

--- ft_puts.s
+++ ft_puts.s
@@ -7,6 +7,10 @@ ft_puts:
 	mov r9, 0
 	mov r10, rdi
 
+check_null:
+	cmp byte [r8], 0
+	je new_line
+
 get_len:
 	add r9, 1
 	inc r8
@@ -15,11 +19,21 @@ get_len:
 
 output:
 	mov rax, 0x2000004
-	;add r9, 1
-	;add r10, 10
 	mov rdi, 1
 	mov rsi, r10
 	mov rdx, r9
 	syscall
+
+new_line:
+	mov rax, 0x2000004
+	mov rdi, 1
+	lea rsi, [rel msg]
+	mov rdx, 1
+	syscall
+
+ret0:
 	mov rax, r9
 	ret
+
+section .data
+	msg: db "", 10
