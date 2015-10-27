ad50d63341a00d8f3cf53d3aa9d48f0e4e3f2c1a
Emil MAMMADZADA <emammadz@e3r9p6.42.fr>
2015-10-27 15:54:10 +0100

    added non wporking strdup again

--- ft_strdup.s
+++ ft_strdup.s
@@ -8,23 +8,24 @@ extern malloc
 ft_strdup:
 	cmp rdi, 0
 	je null
-	mov r8, rdi
+	mov r10, rdi
+
 	call ft_strlen
+	imul rax, 6
 	mov r9, rax
-	inc r9
+
 	mov rdi, r9
+	mov rdi, 18
+	push 18
 	call malloc
 	cmp rax, 00
 	je null
-	mov rdi, [rsp]
-	mov rcx, r9 
-	mov r9, rax
+	mov rcx, 1
 	mov rdi, rax
-	mov rsi, r8
+	mov rsi, r10
 	cld
 	rep movsb
 	mov byte [rdi], 0
-	mov rax, r8
 	ret
 null:
 	mov rax, 00
